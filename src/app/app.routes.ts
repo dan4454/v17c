9d6faeeb1ea5b7ee271b9eb596554a0793fb8880
DanP <danpr987@gmail.com>
2024-07-08 14:03:49 -0400

    asf

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,30 +1,28 @@
-import { NoPreloading, Routes } from '@angular/router';
-import { Parent1Component } from './parent1/parent1.component';
-import { DeferAndIfComponent } from './defer-and-if/defer-and-if.component';
-import { ParentNewInputComponent } from './parent-new-input/parent-new-input.component';
+import { Route } from '@angular/router';
+// import { DeferAndIfComponent } from './defer-and-if/defer-and-if.component';
+// import { ParentNewInputComponent } from './parent-new-input/parent-new-input.component';
+// import { Parent1Component } from './parent1/parent1.component';
+// import { DeferAndIfComponent } from './defer-and-if/defer-and-if.component';
 
-export const routes: Routes = [
+export const routes: Route[] = [
   {
     path: 'parent1',
     loadComponent: () =>
-      import('./parent1/parent1.component').then((m) => Parent1Component),
+      import('./parent1/parent1.component').then(m => m.Parent1Component),
   },
 
   {
     path: 'deferandif',
     loadComponent: () =>
       import('./defer-and-if/defer-and-if.component').then(
-        (m) => DeferAndIfComponent
+        m => m.DeferAndIfComponent
       ),
   },
   {
     path: 'newinput',
     loadComponent: () =>
       import('./parent-new-input/parent-new-input.component').then(
-        (m) => ParentNewInputComponent
+        m => m.ParentNewInputComponent
       ),
   },
-
-  
-  
 ];
